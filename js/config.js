409a08755dae0978671f1ad5e13244fd35ec5bb4
cmoreno-mg <carlos.moreno@warrcloud.com>
2024-03-28 14:26:11 -0600

    new config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "us-east-1_nEP10MQE4",
+    userPoolClientId: "7pcv9ik6kj0iefcg5fbuso595u",
+    region: "us-east-1",
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
