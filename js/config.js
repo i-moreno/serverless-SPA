f9ae03511b2ec7d2bc610522d50f3bfbc562710f
cmoreno-mg <carlos.moreno@warrcloud.com>
2024-03-28 14:58:14 -0600

    update cognito settings

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_nEP10MQE4",
-    userPoolClientId: "7pcv9ik6kj0iefcg5fbuso595u",
+    userPoolId: "us-east-1_lvirKb8Tj",
+    userPoolClientId: "7cbu6ld4g5sajulgjueilv68m3",
     region: "us-east-1",
   },
   api: {
